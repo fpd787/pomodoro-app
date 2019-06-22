de39384da2b36a0d1de22d40e0cf206e9bbb00f3
fpd787 <strangejourney28@gmail.com>
2019-06-22 10:10:29 +0900

    CountDownコンポーネントを作成

---
+++ src/components/CountDown.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>{{ count }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 1500, // 25分
+      timer: null, // setInterval()に格納するオブジェクト
+      isTimerOn: false // タイマーがONのとき(カウントダウンしているとき)にtrue
+    }
+  },
+  methods: {
+    start() {
+      this.timer = setInterval(() => --this.count, 1000)
+      this.isTimerOn = true
+    },
+    pause() {
+      clearInterval(this.timer)
+      this.isTimerOn = false
+    },
+    reset() {
+      clearInterval(this.timer)
+      this.count = 1500
+      this.isTimerOn = false
+    }
+  }
+}
+</script>
