74f0a7aaa33434bcbfde175973c0253e5d75e997
fpd787 <strangejourney28@gmail.com>
2019-06-22 10:10:16 +0900

    ResetButtonコンポーネントを作成

---
+++ src/components/ResetButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <a href="#" class="reset">
+    <span>Reset</span>
+  </a>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.reset {
+  display: inline-block;
+  font-weight: bold;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  color: #fff;
+  background: #ff7f7f;
+  border-radius: 3px;
+  transition: .4s;
+
+  > span {
+    border-bottom: solid 2px #fff;
+    border-top: solid 2px #fff;
+  }
+
+  &:hover > span {
+    padding: 0.1em 0;
+  }
+}
+</style>
