b522ec54c40e7c58543ecf06caec0c3c506aec8f
fpd787 <strangejourney28@gmail.com>
2019-06-22 10:10:10 +0900

    StartButtonコンポーネントを作成

---
+++ src/components/StartButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <a href="#" class="start">Start</a>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.start {
+  display: inline-block;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  background: #668ad8;
+  color: #fff;
+  border-bottom: solid 4px #627295;
+  border-radius: 3px;
+
+  &:active {
+    -webkit-transform: translateY(.4s);
+    transform: translateY(.4s);
+    box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.2);
+    border-bottom: none;
+  }
+}
+</style>
